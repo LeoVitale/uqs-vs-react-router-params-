9418d25922469c70d09fa04345f8562111bfc930
Leonardo Vitale <leonardo.vitale@outlook.com>
2025-09-01 19:33:21 -0300

    Add dropdown menu component and theme toggle functionality; update package dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,12 @@ import { NuqsAdapter } from 'nuqs/adapters/react-router/v7';
 import Home from './components/Home'
 import UrlParams from './components/UrlParams'
 import Nuqs from './components/Nuqs'
+import { ThemeProvider } from './components/theme-provider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <NuqsAdapter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,6 +19,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/nuqs" element={<Nuqs />} />
         </Routes>
       </NuqsAdapter>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
